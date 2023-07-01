f3fdca579e264c22d0b1763ead5a019f106eec42
Дейнека Павлик <freddik1990@gmail.com>
2023-07-01 17:51:28 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿
+static void Recursive(string path)
+{
+    try
+    {
+        string[] items = Directory.GetFileSystemEntries(path);
+
+        foreach (string item in items)
+        {
+            if (Directory.Exists(item))
+            {
+              
+                Recursive(item);
+            }
+            else
+            {
+               
+                Console.WriteLine(item); 
+            }
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(" error : " + ex.Message);
+    }
+}
+
+string startPath = "C:\\";
+try
+{
+    Recursive(startPath);
+}
+catch (Exception ex)
+{
+    Console.WriteLine("error: " + ex.Message);
+}
